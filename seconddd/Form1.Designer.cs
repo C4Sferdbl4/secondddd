d0d5271c15bbba929695680471e5a3407cc0f0fe
C4Sferdbl4 <tonymoonsay@mail.ru>
2022-09-21 23:16:10 +0500

    форма оранж и длинная

--- seconddd/Form1.Designer.cs
+++ seconddd/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace seconddd
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 218);
+            this.button1.Location = new System.Drawing.Point(521, 163);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(61, 25);
             this.button1.TabIndex = 0;
@@ -46,8 +46,8 @@ namespace seconddd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1089, 373);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
